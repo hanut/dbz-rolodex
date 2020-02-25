5d3541a8cd21312481da54826b0deaccb4d811b3
Hanut Singh Gusain <hanutsingh@gmail.com>
2020-02-25 20:40:48 +0530

    Updates

---
+++ precache-manifest.51d649edff112bb3d12dd90e879eef96.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b52db05598878a6e6bf2b5f7e6065353",
+    "url": "/dbz-rolodex/index.html"
+  },
+  {
+    "revision": "59a1bff9059e13cf1fa3",
+    "url": "/dbz-rolodex/static/css/main.4eb44bab.chunk.css"
+  },
+  {
+    "revision": "d63527e02476886396dc",
+    "url": "/dbz-rolodex/static/js/2.894a126c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/dbz-rolodex/static/js/2.894a126c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59a1bff9059e13cf1fa3",
+    "url": "/dbz-rolodex/static/js/main.b14079c2.chunk.js"
+  },
+  {
+    "revision": "c6bc5f79135a32511311",
+    "url": "/dbz-rolodex/static/js/runtime-main.96aeaa15.js"
+  },
+  {
+    "revision": "cb23661943ffff49e02e3d13d3ccb292",
+    "url": "/dbz-rolodex/static/media/logo.cb236619.svg"
+  }
+]);
